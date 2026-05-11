da95d87cbdb16866f399ac349913c7d01b4a46e7
ANORA\ranjith.n <Anora>
2026-05-11 15:30:51 +0530

    Class as Dependency

--- main.py
+++ main.py
@@ -21,6 +21,17 @@ async def get_user(user_name: str, common_dict = Depends(common_operation)):
     common_dict.update({"user_name":user_name})
     return common_dict
 
+#Class as Dependency
+class Item:
+    def __init__(self, item_id: int, item_name: str, price: float):
+        self.item_id = item_id
+        self. item_name = item_name
+        self.price = price
+
+@app.post("/post_item/{item_id}")
+async def post_item(common: Item = Depends(Item)):
+    return common
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
