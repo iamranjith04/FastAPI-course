b53cc7ccb6f51b28ba46bc7e871230f934c780d5
ANORA\ranjith.n <Anora>
2026-05-11 12:56:19 +0530

    Introduction to dependencies

--- main.py
+++ main.py
@@ -1,40 +1,25 @@
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Depends
 import uvicorn
-from starlette.status import HTTP_204_NO_CONTENT
-from pydantic import BaseModel
+
 
 app=FastAPI()
 
-@app.get("/",
-         tags=["Get"] ,
-         summary="my first hello world get route",
-         description="this my first fast api i build for studty purpose",
-         status_code=HTTP_204_NO_CONTENT)
-async def helloWorld():
-    return {"Message": "hello world my first web page"}
+#Dependencies
+async def common_operation(message: str,q:str|None = None):
+    res = {"message" : message}
+    if q:
+        res.update({"q": q})
+    return res
 
-@app.put("/put", tags=["Put"])
-async def put():
-    """
-    This is a put function example
-    - **point 1**: point 1 description
-    - **point 2**: point 2 description
-    :return:
-    """
-    return {"message": "hello from put"}
+@app.get("/items")
+async def get_items(item_name: str, common_dict = Depends(common_operation)):
+    common_dict.update({"item_name":item_name})
+    return common_dict
 
-@app.get("/items/{item_id}/{item_name}", tags=["Get"] )
-async def return_item(item_id: int = Path(..., ge=200 , lt=1000),
-                      item_name: str = Path(..., min_length=1)):
-    return {item_id:item_name}
-
-
-@app.put("/addUser", tags=["Put"])
-async def add_user(name: str = Query("Guest", min_length=3, max_length=10),
-                   password: str= Query(..., min_length=8)):
-    return {name:password}
-
-
+@app.get("/users")
+async def get_user(user_name: str, common_dict = Depends(common_operation)):
+    common_dict.update({"user_name":user_name})
+    return common_dict
 
 
 if __name__ == '__main__':
